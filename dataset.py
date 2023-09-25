32a58f623c216d6e10de4310ee267ad782f758b5
artmerinov <artemmerinov@gmail.com>
2023-09-25 19:46:39 +0200

    refactoring

--- dataset.py
+++ dataset.py
@@ -43,11 +43,6 @@ def train_test_split(data, config):
     return tr_data, va_data
 
 
-def calc_causal_mask(size):
-    mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
-    return mask == 0
-
-
 class RIIIDDataset(Dataset):
     def __init__(self, dataset, config):
         super().__init__()
@@ -64,20 +59,22 @@ class RIIIDDataset(Dataset):
         user_df = self.dataset.loc[user_id]
         seq_len = len(user_df)
 
-        user_e = 1 + torch.from_numpy(user_df['content_id'].values)
-        user_c = 1 + torch.from_numpy(user_df['answered_correctly'].values)
-        user_a = 1 + torch.from_numpy(user_df['user_answer'].values)
+        # since we will add zero padding add 1 to each id
+        # not to mix padding id with potential zero id
+        user_ex = 1 + torch.from_numpy(user_df['content_id'].values)
+        user_ac = 1 + torch.from_numpy(user_df['answered_correctly'].values)
+        user_ua = 1 + torch.from_numpy(user_df['user_answer'].values)
 
-        e = torch.zeros(size=(self.config.MAX_LEN,), dtype=torch.int32) # exercise id
-        c = torch.zeros(size=(self.config.MAX_LEN,), dtype=torch.int32) # correctness (True, False)
-        a = torch.zeros(size=(self.config.MAX_LEN,), dtype=torch.int32) # user answer (A, B, C, D)
+        ex = torch.zeros(size=(self.config.MAX_LEN,), dtype=torch.int32) # exercise id
+        ac = torch.zeros(size=(self.config.MAX_LEN,), dtype=torch.int32) # correctness (True, False)
+        ua = torch.zeros(size=(self.config.MAX_LEN,), dtype=torch.int32) # user answer (A, B, C, D)
 
         if seq_len < self.config.MAX_LEN:
 
-            # add padding in front
-            e[-seq_len:] = user_e
-            c[-seq_len:] = user_c
-            a[-seq_len:] = user_a
+            # add padding in front of sequence
+            ex[-seq_len:] = user_ex
+            ac[-seq_len:] = user_ac
+            ua[-seq_len:] = user_ua
 
         elif seq_len > self.config.MAX_LEN:
 
@@ -85,38 +82,42 @@ class RIIIDDataset(Dataset):
             st = np.random.randint(low=0, high=seq_len - self.config.MAX_LEN)
             en = st + self.config.MAX_LEN
 
-            e[:] = user_e[st:en]
-            c[:] = user_c[st:en]
-            a[:] = user_a[st:en]
+            ex[:] = user_ex[st:en]
+            ac[:] = user_ac[st:en]
+            ua[:] = user_ua[st:en]
 
         else:
 
-            e[:] = user_e
-            c[:] = user_c
-            a[:] = user_a
+            ex[:] = user_ex
+            ac[:] = user_ac
+            ua[:] = user_ua
             
+        assert ex.size(0) == self.config.MAX_LEN
+        assert ac.size(0) == self.config.MAX_LEN
+        assert ua.size(0) == self.config.MAX_LEN
 
-        assert e.size(0) == self.config.MAX_LEN
-        assert c.size(0) == self.config.MAX_LEN
-        assert a.size(0) == self.config.MAX_LEN
-
-        src_mask = (e[1:] != 0).int().unsqueeze(0).unsqueeze(0)
-
-        tgt_mask = (e[:-1] != 0).int().unsqueeze(0).unsqueeze(0)
-        tgt_mask = tgt_mask & calc_causal_mask(e.size(0) - 1)
+        src_mask = (ex[1:] != 0).int().unsqueeze(0).unsqueeze(0)
+        
+        tgt_mask = (ex[:-1] != 0).int().unsqueeze(0).unsqueeze(0)
+        tgt_mask = tgt_mask & calc_causal_mask(ex.size(0) - 1)
 
         return {
             # encoder input (current)
-            "e": e[1:],
+            "ex": ex[1:],
 
             # decoder input (history)
-            'c': c[:-1],
-            'a': a[:-1],
+            'ac': ac[:-1],
+            'ua': ua[:-1],
 
             # label
-            'label': c[1:],
+            'label': ac[1:],
 
             # masks
             'src_mask': src_mask,
             'tgt_mask': tgt_mask,
         }
+
+
+def calc_causal_mask(size):
+    mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
+    return mask == 0
